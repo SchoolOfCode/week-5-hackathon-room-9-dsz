dad8bc93530188cf7c609eadb28d5c26f1ba8c54
Shanti Rai <shantirai@MacBookAir.lan>
2025-01-18 18:09:49 +0000

    created and finished a series of tests for fetchAllMovies() that checks the return object is as it should be. I can still do more for this funciton, I just meant, i have managed to finish this sub-section.

--- tests/models_tests/movies.test.js
+++ tests/models_tests/movies.test.js
@@ -1,17 +1,48 @@
-import { describe, test, expect, assert } from "vitest";
+import { describe, test, expect, beforeEach, afterEach } from "vitest";
 import { fetchAllMovies } from "../../models/movies";
 import { pool } from "../../db/index.js";
 
 describe("fetchAllMovies", () => {
-  test("SQL command returns a JSON object", async () => {
+  beforeEach(async () => {
+    // Delete dependent reviews first
+    await pool.query("DELETE FROM reviews WHERE movies_id IS NOT NULL");
+    // Set up the database for each test case -empty the movies table before each test
+    await pool.query("DELETE FROM movies");
+  });
+
+  test("SQL command returns succesfully with multiple movie objects", async () => {
     //arrange
-    const query = "SELECT * FROM movies";
+    // Insert multiple movie records into the database before running the query
+    await pool.query(`
+        INSERT INTO movies (movie_name, release_date, box_office_gross, lead_actor, director) VALUES
+        ('Amores Perros', '2000-05-19', 20940000, 'Gael García Bernal', 'Alejandro González Iñárritu'),
+        ('Birdman', '2014-10-17', 103215094, 'Michael Keaton', 'Alejandro González Iñárritu')
+      `);
     //act
-    const result = await pool.query(query);
-    console.log("Hello:", result);
+    const result = await fetchAllMovies();
     //assert
-    // Assert
-    expect(Array.isArray(result.rows)).toBe(true);
-    expect(result.rows).toBeDefined();
+    expect(Array.isArray(result)).toBe(true); // result is an array
+    expect(result).toBeDefined(); // Ensure result is defined
+  });
+  test("SQL command returns an empty array is one movie in the database", async () => {
+    // act
+    await pool.query(`
+        INSERT INTO movies (movie_name, release_date, box_office_gross, lead_actor, director) VALUES
+        ('Amores Perros', '2000-05-19', 20940000, 'Gael García Bernal', 'Alejandro González Iñárritu')
+      `);
+    const result = await fetchAllMovies();
+    // assert
+    expect(Array.isArray(result)).toBe(true);
+    expect(result).toHaveLength(1); // Ensure the array has one value
+    expect(result).toBeDefined();
+  });
+  test("SQL command returns an empty array when no movies are in the database", async () => {
+    // act
+    const result = await fetchAllMovies();
+
+    // assert
+    expect(Array.isArray(result)).toBe(true);
+    expect(result).toHaveLength(0); // Ensure the array is empty when no data is found
+    expect(result).toBeDefined();
   });
 });
